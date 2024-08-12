d764a3c56d1918ca7170e50ab3e5e0a4f1666e49
Ercan Ünal <ercan.unal@uniconn.com.tr>
2024-08-12 16:33:38 +0300

    init project

---
+++ src/main/java/com/definex/springstatemachinedefinex/SpringStatemachineDefinexApplication.java
@@ -0,0 +1,13 @@
+package com.definex.springstatemachinedefinex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStatemachineDefinexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStatemachineDefinexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/definex/springstatemachinedefinex/SpringStatemachineDefinexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.definex.springstatemachinedefinex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStatemachineDefinexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
